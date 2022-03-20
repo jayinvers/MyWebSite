08b67d208c092f84807f472bfeacabbf206a238c
Jay Liu <maxazure@gmail.com>
2022-03-21 11:31:13 +1300

    Edit: move MywebSite.db to /db folder

--- MyWebSite/Program.cs
+++ MyWebSite/Program.cs
@@ -18,11 +18,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
+/*using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     SeedData.Initialize(services);
-}
+}*/
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
